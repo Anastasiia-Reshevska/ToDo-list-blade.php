0c2266beddf7804c7f5b334e5e19906d4b1cfd9f
Anastasiia-Rechevska <volokh.qs@gmail.com>
2024-09-27 09:31:09 +0300

    Made a hover for the button background, added a todo list section,

--- resources/views/components/todo-list/todo-list.blade.php
+++ resources/views/components/todo-list/todo-list.blade.php
@@ -1,31 +1,33 @@
-<div class="to-do-list__container">
-    <div class="to-do-list__content">
-        <div class="to-do-list__title">
-            <h1>To-Do list</h1>
-            <h3>Главная задача на день, спланировать его. Планируй вместе с нами!</h3>
-            <h4>Список дел</4h>
-        </div>
-        <ul class="to-do-list__affairs">
-            <li>
-                Купить молоко
-                <div class="to-do-list__check">
-                    <button class="to-do-list__check_background">
-                        <div class="to-do-list__check_mark"></div>
-                    </button>
+<section class="to-do-list">
+    <div class="to-do-list__container">
+        <div class="to-do-list__content">
+            <div class="to-do-list__title">
+                <h1>To-Do list</h1>
+                <h3>Главная задача на день, спланировать его. Планируй вместе с нами!</h3>
+                <h4>Список дел</4h>
+            </div>
+            <ul class="to-do-list__affairs">
+                <li>
+                    Купить молоко
+                    <div class="to-do-list__check">
+                        <button class="to-do-list__check_background">
+                            <div class="to-do-list__check_mark"></div>
+                        </button>
 
-                    <button class="to-do-list__check_background">
-                        <div class="to-do-list__cross"></div>
-                    </button>
-                </div>
-            </li>
-        </ul>
-    </div>
-    <div class="to-do-list__content">
-        <h4>Добавить новую задачу</h4>
-        <div>
-            <input type="text" placeholder="Текст задачи">
+                        <button class="to-do-list__check_background">
+                            <div class="to-do-list__cross"></div>
+                        </button>
+                    </div>
+                </li>
+            </ul>
         </div>
-        <p>Что делаем, сколько времени тратим, какой результат получаем</p>
-        <button class="to-do-list__add_task">Добавить</button>
+        <div class="to-do-list__content">
+            <h4>Добавить новую задачу</h4>
+            <div>
+                <input type="text" placeholder="Текст задачи">
+            </div>
+            <p>Что делаем, сколько времени тратим, какой результат получаем</p>
+            <button class="to-do-list__add_task">Добавить</button>
+        </div>
     </div>
-</div>
+</section>
