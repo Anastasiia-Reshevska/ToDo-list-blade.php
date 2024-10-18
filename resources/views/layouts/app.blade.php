8b67ddf995c081a4ac1cd524192ff3ae620bddac
Anastasiia-Rechevska <volokh.qs@gmail.com>
2024-10-18 11:49:55 +0300

    added a placeholder to the tag, reduced the hundredths after the decimal point to tenths, reduced the block size, centered the elements, and removed the margin from footer__navigation.

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -7,7 +7,6 @@
     <title>@yield('title')</title>
     <link rel="preconnect" href="https://fonts.googleapis.com">
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
-    <link href="https://fonts.googleapis.com/css2?family=Bonheur+Royale&family=Lato:ital,wght@0,100;0,300;0,400;0,700;0,900;1,100;1,300;1,400;1,700;1,900&display=swap" rel="stylesheet">
     @vite(['resources/scss/pages/home.scss', 'resources/js/pages/home.js',])
 </head>
 
